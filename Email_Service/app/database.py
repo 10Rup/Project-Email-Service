b7e28e17955d84e740a5139db86a7bd24b5f8a0c
10Rup <rupmandal.lspl@gmail.com>
2026-04-01 17:33:10 +0530

    updated database
    batabase url updted

--- Email_Service/app/database.py
+++ Email_Service/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:root@localhost/emailservice"
+DATABASE_URL = "postgresql://postgres:password@localhost/email_service"
 
 engine = create_engine(DATABASE_URL)
 
